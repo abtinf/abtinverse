6a57b82e312211f236c02d9274c8bcff6316fb52
Abtin Forouzandeh <abtinf@gmail.com>
2017-07-27 10:42:09 -0700

    Initial custom menu bar

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const {app, BrowserWindow, Menu} = require('electron');
 const baseURL = 'https://mail.notes.na.collabserv.com/';
+const emailURL = baseURL + "verse?";
 const userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.35 Safari/537.36';
 const initialWidth = 1280, initialHeight = 720;
 
@@ -10,7 +11,26 @@ function createWindow() {
 		//https://github.com/electron/electron/issues/7047
 		webPreferences: { nodeIntegration: false }
 	});
-	win.loadURL(baseURL,
+	const template = [{
+		label: 'Development',
+		submenu: [{
+			label: 'Dev Tools',
+			accelerator: 'Alt+Cmd+I',
+			click() {
+				win.toggleDevTools();
+			}
+		}]
+	}];
+	if(process.platform === 'darwin') {
+		template.unshift({
+			label: app.getName(),
+			submenu: [
+				{role: 'quit'}
+			]
+		});
+	}
+	Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+	win.loadURL(emailURL,
 		{userAgent: userAgent});
 }
 
